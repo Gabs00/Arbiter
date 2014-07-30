405f7f0e529eed3e56f3579adb5808bcf9362720
Gabs00 <gewen87@gmail.com>
2014-07-30 03:52:34 -0400

    Added test folder, in that added Ranger.js, Ranger class, and test.js for the test program

---
+++ Test/Ranger.js
@@ -0,0 +1,25 @@
+
+var Ranger = function(x,y,name, arbiter){
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.id = null;
+    this.arbiter = arbiter;
+    this.range = [[0, 200, 'attack']];
+}
+
+Ranger.prototype.attack = function(target){
+    console.log('===ATTACK===')
+    console.log(this.name + ' attacks ' + target.name);
+    console.log('============');
+}
+
+Ranger.prototype.updateLocation = function(){
+    this.x = Math.floor(Math.random()*400);
+    this.y = Math.floor(Math.random()*400);
+    this.arbiter.updateLocation({'id':this.id, 'location': [this.x, this.y]});    
+}
+
+Ranger.prototype.ranges = function(){
+    return this.range;
+}
