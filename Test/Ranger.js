708e476d7938bf823db8eda1462eec3fc98b9e3a
Gabs00 <gewen87@gmail.com>
2014-08-02 00:46:17 -0400

    Added semicolons to ranger methods.

--- Test/Ranger.js
+++ Test/Ranger.js
@@ -8,20 +8,20 @@ var Ranger = function(x,y,name, arbiter){
     this.id = null;
     this.arbiter = arbiter;
     this.range = [[0, 200, 'attack']];
-}
+};
 
 Ranger.prototype.attack = function(target){
-    console.log('===ATTACK===')
+    console.log('===ATTACK===');
     console.log(this.name + ' attacks ' + target.name);
     console.log('============');
-}
+};
 
 Ranger.prototype.updateLocation = function(){
     this.x = Math.floor(Math.random()*400);
     this.y = Math.floor(Math.random()*400);
-    this.arbiter.updateLocation({'id':this.id, 'location': [this.x, this.y]});    
-}
+    this.arbiter.updateLocation({ id:this.id, location: [this.x, this.y]});    
+};
 
 Ranger.prototype.ranges = function(){
     return this.range;
-}
+};
